5ab956838d610dd568c65bdf941fbafb88524aa2
sebastianpennino <sebastian.pennino@gmail.com>
2023-07-29 03:16:37 -0300

    Starting adding types

---
+++ src/typings/Domains.ts
@@ -0,0 +1,48 @@
+export enum PowerWordDomain {
+  ABJURATION = "ABJURATION",
+  CONJURATION = "CONJURATION",
+  DIVINATION = "DIVINATION",
+  ENCHANTMENT = "ENCHANTMENT",
+  EVOCATION = "EVOCATION",
+  ILUSION = "ILUSION",
+  NECROMANCY = "NECROMANCY",
+  TRANSMUTATION = "TRANSMUTATION",
+}
+
+export type ValidPowerWordDomain =
+  (typeof PowerWordDomain)[keyof typeof PowerWordDomain];
+
+export const domainList = [
+  {
+    name: ["Abjuración", "Abjuration"],
+    formulaName: PowerWordDomain.ABJURATION,
+  },
+  {
+    name: ["Conjuración", "Conjuration"],
+    formulaName: PowerWordDomain.CONJURATION,
+  },
+  {
+    name: ["Divinación", "Divination"],
+    formulaName: PowerWordDomain.DIVINATION,
+  },
+  {
+    name: ["Encantamiento", "Enchantment"],
+    formulaName: PowerWordDomain.ENCHANTMENT,
+  },
+  {
+    name: ["Evocación", "Evocation"],
+    formulaName: PowerWordDomain.EVOCATION,
+  },
+  {
+    name: ["Ilusión", "Ilusion"],
+    formulaName: PowerWordDomain.ILUSION,
+  },
+  {
+    name: ["Necromancia", "Necromancy"],
+    formulaName: PowerWordDomain.NECROMANCY,
+  },
+  {
+    name: ["Transmutación", "Transmutation"],
+    formulaName: PowerWordDomain.TRANSMUTATION,
+  },
+];
